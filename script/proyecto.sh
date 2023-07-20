fd69fd170dfb617d166ac8826036817fe90ea87c
Francisco Prestamo <prestamo2003@gmail.com>
2023-07-19 22:56:02 -0600

    Update proyecto.sh

--- script/proyecto.sh
+++ script/proyecto.sh
@@ -16,6 +16,7 @@ function clean {
     cd ".."
     find . -type d -name bin -exec rm -rf {} +
     find . -type d -name obj -exec rm -rf {} +
+    find . -type d -name .vs -exec rm -rf {} +
 
     echo "Proyecto limpiado."
 }
